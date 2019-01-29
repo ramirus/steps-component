964ae5760d7420745d5e0cc86dc57389246034e1
Ramiru <blackhawk111@yandex.ru>
2019-01-30 00:15:42 +0300

    add steps render functions and helpers

--- src/components/Steps/index.js
+++ src/components/Steps/index.js
@@ -1,9 +1,50 @@
 import React from 'react'
+import Step from './Step'
 
-const Steps = () => {
+const Steps = (props) => {
+
+  const isActive = (index) => Math.abs(index - props.current)
+
+  const handleClick = (index) => {
+    if (isActive(index)) {
+      this.props.handleClick(index)
+    }
+  }
+
+  const renderChildren = () => {
+    const { current, steps } = props;
+
+    const stepLengtn = steps.length;
+    if (stepLengtn < 2) {
+      console.warn("In Steps component must be more than 2 and max 5 steps");
+      return null;
+    }
+    if (stepLengtn > 2) {
+      console.warn("In Steps component must be max 5 steps");
+    }
+
+    const result = [];
+    
+    for (let i = 0; i < stepLengtn && i < 5; i++) {
+      result.push(
+        <Step
+          key={steps[i].label}
+          title={steps[i].label}
+          handleClick={handleClick}
+          current={current === i}
+          active={isActive(i)}
+          checked={i < current}
+        />
+      )
+    }
+
+    return result;
+  }
+  
+
   return (
     <div>
-      
+      { renderChildren() }
     </div>
   )
 }
